8db66f866bc644dfd8297de8de8cce8bdc6e2c7a
Delovinde <lennaertpott@gmail.com>
2019-04-01 07:08:30 -0400

    Starting from local files

--- src/com/engine/calc/MathProcessing.java
+++
@@ -1,8 +0,0 @@
-package com.engine.calc;
-
-public interface MathProcessing {
-    String SEPARATOR = " ";
-    String getKeyword();
-    char getSymbol();
-    double doCalculation(double leftVal, double rightVal);
-}
